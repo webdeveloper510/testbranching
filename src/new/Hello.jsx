855e81b895d76a37bf0bdd728d299d6e2d734911
webdeveloper <webdeveloper510>
2023-03-17 12:41:01 +0530

    2ndcommit

--- src/new/Hello.jsx
+++ src/new/Hello.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hello = () => {
   return (
-    <div>Hello Aggain</div>
+    <div> create mode 100644 src/new/Hello.jsx
+    PS C:\Users\Anjali\OneDrive\Desktop\testingApp2\c git push origin main
+    error: src refspec main does not match any
+    error: failed to push some refs to 'https://github.com/webdeveloper510/testbranching.git'
+    PS C:\Users\Anjali\OneDrive\Desktop\testingApp2\c git push origin test
+    Total 0 (delta 0), reused 0 (delta 0), pack-reused 0
+    To https://github.com/webdeveloper510/testbranching.git
+       323d101..75a80b4  test test
+    PS C:\Users\Anjali\OneDrive\Desktop\testingApp2\c
+    </div>
   )
 }
 
