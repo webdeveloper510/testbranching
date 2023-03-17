353a491b3b4a4bbb20363a74601c80a569e134b7
webdeveloper <webdeveloper510>
2023-03-17 11:43:29 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Hello from './view/hello'
+import About from './view/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Hello/>
+      <Hello />
+      <About />
     </div>
   )
 }
