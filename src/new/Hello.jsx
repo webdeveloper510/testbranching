0d798b24b9e1e1a8405655cf559e80143ad12b68
webdeveloper <webdeveloper510>
2023-03-17 12:53:18 +0530

    2ndcommit

--- src/new/Hello.jsx
+++ src/new/Hello.jsx
@@ -4,7 +4,7 @@ const Hello = () => {
   return (
     <div>What is content testing?
     Content testing is a form of UX research designed to assess content quality and performance. It answers questions such as:
-    
+    the branchhhhhh
     Does this content resonate with your target audience, addressing their biggest pain points and desires?
     Is this content actionable and easy for consumers to implement?
     What aspects of the content, if any, create unnecessary friction or frustration?
