c0b0eb0c24c22e65349dd675f6a5d2f7f089a748
webdeveloper <webdeveloper510>
2023-03-17 12:59:48 +0530

    newww

--- src/new/Hello.jsx
+++ src/new/Hello.jsx
@@ -8,8 +8,8 @@ const Hello = () => {
     Does this content resonate with your target audience, addressing their biggest pain points and desires?
     Is this content actionable and easy for consumers to implement?
     What aspects of the content, if any, create unnecessary friction or frustration?
-    As a result, it helps content strategists (and marketers in general) to improve user experience and, ultimately, generate better results from content. What kinds of results?</div>
-    <div> create mode 100644 src/new/Hello.jsx
+    As a result, it helps content strategists (and marketers in general) to improve user experience and, ultimately, generate better results from content. What kinds of results?
+create mode 100644 src/new/Hello.jsx
     PS C:\Users\Anjali\OneDrive\Desktop\testingApp2\c git push origin main
     error: src refspec main does not match any
     error: failed to push some refs to 'https://github.com/webdeveloper510/testbranching.git'
